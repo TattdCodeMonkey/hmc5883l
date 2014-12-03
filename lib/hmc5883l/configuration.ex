5cb9432635fd22f4b0dc9296952c583165ad7c74
Rodney Norris <Rodney.Norris@gmail.com>
2014-12-02 22:23:23 -0600

    added struct module for the configuration

---
+++ lib/hmc5883l/configuration.ex
@@ -0,0 +1,7 @@
+defmodule HMC5883L.Configuration do
+  defstruct averaging: 8, data_rate: 15, bias: :normal, gain: 1.3, mode: :continuous, i2c_channel: 'i2c-0', i2c_devid: 0x1E 
+  alias __MODULE__
+  
+  def new(), do: %Configuration{}
+
+end 
