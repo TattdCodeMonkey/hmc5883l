fb06c525aa2b3d12c6d0a5db8056bd103dc8b1b1
Rodney Norris <Rodney.Norris@gmail.com>
2014-12-08 22:46:34 -0600

    corrected error with handle_calls

--- lib/hmc5883l/headingserver.ex
+++ lib/hmc5883l/headingserver.ex
@@ -19,9 +19,9 @@ defmodule HMC5883L.HeadingServer do
   #####
   # GenServer Implementation
 
-  def handle_call(:get_value, _from, current_state), do: {:reply, current_state.heading, current_state }
+  def handle_call({:get_value}, _from, current_state), do: {:reply, current_state.heading, current_state }
 
-  def handle_call(:get_state, _, current_state), do: {:reply, current_state.state, current_state}
+  def handle_call({:get_state}, _, current_state), do: {:reply, current_state.state, current_state}
 
   def handle_cast({:update_value, value}, current_state) do
     current_state = %{current_state| heading: value}
@@ -36,5 +36,5 @@ defmodule HMC5883L.HeadingServer do
   #####
   # private method
   defp _init(), do: %{heading: 0.0, state: :unknown}
-  
+
 end
